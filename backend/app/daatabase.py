e6deb43c9f4aeba2e31bb407a9fa873f3fd12524
vishalkrpal-cse <vishalkrpal3@gmail.com>
2026-03-29 08:36:29 +0530

    Delete backend/app/daatabase.py

--- backend/app/daatabase.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = "postgresql://postgres:password@localhost/doc_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-
-Base = declarative_base()
